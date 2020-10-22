1ac8484c3d1ba28ea86d09f9ac813e18198318e0
KiWi Developer <kiwifinance.bsc@gmail.com>
2020-10-22 10:03:06 +0600

    cheeseswap-sdk

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  BSCTESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x173C66eee973AF865753266F19b982Bfc91Ab609'
+
+export const INIT_CODE_HASH = '0x1b3885aa75fe669881dd28686a25831c0dd76528f57f2791bf1872bb665f9782'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real CheeseSwapPair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0x1edce9c116cf81aa43c27184471b370ad8e00a946bbb55fa082b1a3ad3ca02f1')
+    })
+  })
+})
